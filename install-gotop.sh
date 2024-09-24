868b916705f747fe51f1dbea6a8f789c22a30cfc
ftreff <ftreff@gmail.com>
2024-09-24 03:52:06 -0400

    Update install-gotop.sh

--- install-gotop.sh
+++ install-gotop.sh
@@ -1,6 +1,6 @@
 cd /tmp
-wget https://github.com/xxxserxxx/gotop/releases/download/v4.2.0/gotop_v4.2.0_linux_arm64.rpm
-yes | sudo apt install /tmp/gotop_v4.2.0_linux_arm64.rpm
-rm gotop_v4.2.0_linux_arm64.rpm
+wget https://github.com/xxxserxxx/gotop/releases/download/v4.2.0/gotop_v4.2.0_linux_arm64.deb
+yes | sudo apt install /tmp/gotop_v4.2.0_linux_arm64.deb
+rm gotop_v4.2.0_linux_arm64.deb
 cd
 echo "gotop is now installed"
